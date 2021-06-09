1ff02df8f4dde2909308d8217706e316c4142911
DhKim <welcome2c@mobidoo.co.kr>
2021-06-09 13:49:47 +0900

    modify dialog background round

--- lottieProegressDialog/src/main/java/com/devhoony/lottieproegressdialog/LottieProgressDialog.kt
+++ lottieProegressDialog/src/main/java/com/devhoony/lottieproegressdialog/LottieProgressDialog.kt
@@ -2,8 +2,11 @@ package com.devhoony.lottieproegressdialog
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.Window
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.airbnb.lottie.LottieAnimationView
@@ -14,16 +17,16 @@ class LottieProgressDialog(
     // setting cancelable
     private val isCancel: Boolean,
     // if you want change dialog size input value not null
-    private val dialogWidth: Int? = 200,
-    private val dialogHeight: Int? = 180,
+    private val dialogWidth: Int?,
+    private val dialogHeight: Int?,
     // if you want change animation size input value not null
-    private val animationViewWidth: Int? = 90,
-    private val animationViewHeight: Int? = 90,
+    private val animationViewWidth: Int?,
+    private val animationViewHeight: Int?,
     // if you want change animation in samples refer to companion object
     // if you want specific file input file name not null
     private val fileName: String,
     // if you want change title input string not null
-    private val title: String? = "Loading...",
+    private val title: String?,
     // if you want change title visible input visible value not null
     private val titleVisible: Int? = View.VISIBLE
 ) :
@@ -35,6 +38,8 @@ class LottieProgressDialog(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        window?.requestFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.dialog_lottie_progress)
 
         setCancelable(isCancel)
